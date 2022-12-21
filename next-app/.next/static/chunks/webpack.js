9f897f1dbcc8a918f82d26c8f02914f7107dcaf1
Reinhardt <reinhardt.cgr@gmail.com>
2022-12-21 21:44:19 +1300

    trying new directory structure to later make nextJS app into chrome extension

--- next-app/.next/static/chunks/webpack.js
+++ next-app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c8ecd947b49ba515"; }
+/******/ 		__webpack_require__.h = function() { return "987c2821aca10aa0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
