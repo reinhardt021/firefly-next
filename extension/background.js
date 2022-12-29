22dd64a763fe5c474b3c0a0dcdd1951dbcfb28d7
Reinhardt <reinhardt.cgr@gmail.com>
2022-12-29 20:46:08 +1300

    removed not working code

--- extension/background.js
+++ extension/background.js
@@ -1,15 +1,4 @@
-chrome.runtime.onInstalled.addListener((reason) => {
-  //alert('INSTALLED');
-  console.log('INSTALLED');
-  if (reason == chrome.runtime.OnInstalledReason.INSTALL) {
-    console.log('reasone match');
-    chrome.tabs.create({ url: 'index.html' });
-  }
-});
 chrome.action.onClicked.addListener(() => {
-  //alert('CLICKED'); // gets hers but erros
-  console.log('CLICKED'); 
-  //chrome.tabs.create({ url: 'chrome://newtab' });
   // when clicking the extension icon, a new tab with the extension is open
   chrome.tabs.create({ url: 'index.html' });
 });
