3c1fe474a27edb61824344c741b52d57198ea49a
Reinhardt <reinhardt.cgr@gmail.com>
2022-12-29 20:33:17 +1300

    have at least the extension clicking working to open the firefly app

--- extension/background.js
+++ extension/background.js
@@ -1,11 +1,22 @@
-chrome.action.onClicked.addListener(() => {
-  alert('TEST');
-  chrome.tabs.create({ url: 'chrome://newtab' });
+chrome.runtime.onInstalled.addListener((reason) => {
+  //alert('INSTALLED');
+  console.log('INSTALLED');
+  if (reason == chrome.runtime.OnInstalledReason.INSTALL) {
+    console.log('reasone match');
+    chrome.tabs.create({ url: 'index.html' });
+  }
 });
-//chrome.runtime.onInstalled.addListener((reason) => {
-  //if (reason == chrome.runtime.OnInstalledReason.INSTALL) {
-    //chrome.tabs.create({
-      //url: 'index.html'
-    //});
-  //}
-//});
+chrome.action.onClicked.addListener(() => {
+  //alert('CLICKED'); // gets hers but erros
+  console.log('CLICKED'); 
+  //chrome.tabs.create({ url: 'chrome://newtab' });
+  // when clicking the extension icon, a new tab with the extension is open
+  chrome.tabs.create({ url: 'index.html' });
+});
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  //alert('MESSAGE');
+  console.log('MESSAGE');
+  if (request.message == 'open_new_tab') {
+  chrome.tabs.create({ url: 'index.html' });
+  }
+});
