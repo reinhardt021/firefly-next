acaa1b7c32feedc0be2dd2ab54f609d8b1d2eb09
Reinhardt <reinhardt.cgr@gmail.com>
2022-12-29 21:56:20 +1300

    removed unneeded code

--- extension/background.js
+++ extension/background.js
@@ -2,10 +2,3 @@ chrome.action.onClicked.addListener(() => {
   // when clicking the extension icon, a new tab with the extension is open
   chrome.tabs.create({ url: 'index.html' });
 });
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  //alert('MESSAGE');
-  console.log('MESSAGE');
-  if (request.message == 'open_new_tab') {
-  chrome.tabs.create({ url: 'index.html' });
-  }
-});
